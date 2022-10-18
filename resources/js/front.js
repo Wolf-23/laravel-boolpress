34b62a4d3f68a46ab4939835004283ae00269a84
Riccardo <lupinu.ric@gmail.com>
2022-10-18 14:42:06 +0200

    Create router.js

--- resources/js/front.js
+++ resources/js/front.js
@@ -1,8 +1,10 @@
 window.axios = require('axios');
 import Vue from 'vue';
 import App from './views/App';
+import router from './router.js';
 
 const app = new Vue({
     el: '#root',
-    render: h => h(App)
+    render: h => h(App),
+    router
 });
